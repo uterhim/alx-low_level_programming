cb5ec093df5a7eb322e35f13b1e4318f57c05227
uterhim <uterhim@gmail.com>
2022-06-14 22:57:38 +0300

    repo with 0 task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"                                                                                                                             
+/**                                                                                                                                           
+ * reset_to_98 - Check pointer                                                                                                                
+ * @n: pointer variable                                                                                                               
+ * Description: function uses to compute pointer                                                                                     
+ * Return: correct                                                                                                   
+ *                                                                                                                                            
+ */                                                                                                                                           
+void reset_to_98(int *n)                                                                                                                           
+{
+        *n = 98;                                                                                                                                                                                                                                                                 
+}
