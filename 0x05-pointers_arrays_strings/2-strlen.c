026adb4aa09fd1e897c202a038da4e8e0eaefa30
uterhim <uterhim@gmail.com>
2022-06-14 19:55:32 -0700

    zero task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - length of character
+ * @s: character variable
+ * Description: length
+ * Return: correct
+ */
+int _strlen(char *s)
+{
+	int i = strlen(s);
+
+	return (i);
+}
