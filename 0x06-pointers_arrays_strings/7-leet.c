7a6453249b2e617ddef95b66c93d6f933689067b
uterhim <uterhim@gmail.com>
2022-06-16 05:31:04 -0700

    task 7 done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * leet - uppercase func
+ * @str: str type variable
+ * Description: function used to change uppercase
+ * Return: success
+ */
+char *leet(char *str)
+{
+	char *ll = "aeotl";
+
+	char *ul = "AEOTL";
+
+	char r[] = {'4', '3', '0', '7', '1'};
+
+	int i;
+
+	char *p = str;
+
+	while (*str != '\0')
+	{
+		for (i = 0; i < 5; i++)
+		{
+			if (*str == *(ll + i) || *str == *(ul + 1))
+				*str = r[i];
+		}
+		str++;
+	}
+	return (p);
+}
