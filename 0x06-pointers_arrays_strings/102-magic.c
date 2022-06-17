81d7cea974cd3ec2e291b3565417dee1a8979dd9
uterhim <uterhim@gmail.com>
2022-06-17 05:19:28 -0700

    magic tas done suc

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,36 +1,21 @@
 #include <stdio.h>
-/*
- * main - main function
- * Description: dispaly specific number
- * Return: Always 0
- */
 
 int main(void)
 {
 	int n;
-
 	int a[5];
-
 	int *p;
 
 	a[2] = 1024;
-
 	p = &n;
-
-		    /*
-		     *
-		     * write your line of code here...
-		     *
-		     * Remember:
-		     *
-		     * you are not allowed to use a
-		     *
-		     * - you are not allowed to modify p
-		     * - only one statement
-		     * - you are not allowed to code anything else
-		     *   than this line of code
-		     *
-		     */
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line code
+	 */
 	 *(p + 5) = 98;
 	 /* ...so that this prints 98\n */
 
