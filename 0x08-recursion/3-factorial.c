b0e24416fcf35e473a197db6ff7dd7de18c736a5
uterhim <uterhim@gmail.com>
2022-06-23 17:19:43 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - function
+ * @n: variable of int
+ * Return: return factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
