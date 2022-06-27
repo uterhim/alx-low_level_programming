8ebc082290349b64517779e047bf7020c8cc122e
uterhim <uterhim@gmail.com>
2022-06-27 00:39:00 -0700

    multiplication done

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,10 +10,15 @@ int main(int argc, char **argv)
 {
 	int mul = 1;
 
-	if (argc < 4)
+	if (argc < 4 && argc == 3)
 	{
 		mul = atoi(*(argv + 1)) * atoi(*(argv + 2));
 		printf("%i\n", mul);
+	}
+	else
+	{
+		printf("Error");
+		return (1);
 	}
 	return (0);
 }
