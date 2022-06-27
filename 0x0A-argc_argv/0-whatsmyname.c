ea9270930f910649970d4712f971401c224dfe12
uterhim <uterhim@gmail.com>
2022-06-27 00:08:08 -0700

    name for task zero

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function
+ * @argc: length of string
+ * @argv: character or string t
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+printf("%s\n", *argv);
+return (0);
+}
