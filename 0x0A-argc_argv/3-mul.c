868d5d1d6556e2f11cb901de93a796e9b6d3d080
uterhim <uterhim@gmail.com>
2022-06-27 00:54:03 -0700

    corrected

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,15 +10,15 @@ int main(int argc, char **argv)
 {
 	int mul = 1;
 
-	if (argc < 4 && argc == 3)
-	{
-		mul = atoi(*(argv + 1)) * atoi(*(argv + 2));
-		printf("%i\n", mul);
-	}
-	else
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error");
 		return (1);
+	}
+	else
+	{
+		mul = atoi(*(argv + 1)) * atoi(*(argv + 2));
+		printf("%i\n", mul);
 	}
 	return (0);
 }
