8a11f0828afb9e8a86d693d9990ec46ba92be390
uterhim <uterhim@gmail.com>
2022-07-04 14:39:43 -0700

    updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,31 +14,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	owner_l = 0;
 	name_l = 0;
-
 	while (name[name_l++])
 		;
 	while (owner[owner_l++])
 		;
 	dog2 = malloc(sizeof(dog_t));
-
 	if (dog2 == NULL)
 		return (NULL);
-
 	dog2->name = malloc(name_l * sizeof(dog2->name));
-
 	if (dog2 == NULL)
 		return (NULL);
-
 	for (i = 0; i < name_l; i++)
 		dog2->name[i] = name[i];
 
 	dog2->age = age;
 
 	dog2->owner = malloc(owner_l * sizeof(dog2->owner));
-
 	if (dog2 == NULL)
 		return (NULL);
-
 	for (i = 0; i < owner_l; i++)
 		dog2->owner[i] = owner[i];
 	return (dog2);
