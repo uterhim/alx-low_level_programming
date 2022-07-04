ca280b4aadd30130ebf82a679af03fcd965400f2
uterhim <uterhim@gmail.com>
2022-07-04 07:10:53 -0700

    structure example in taking dog data

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog data
+ * @name: name of dog variable
+ * @age: age of dog variable
+ * @owner: owner of dog variable
+ * Description: about dog data
+ */
+struct dog
+{
+	char *name;
+	double age;
+	char *owner;
+};
+#endif
